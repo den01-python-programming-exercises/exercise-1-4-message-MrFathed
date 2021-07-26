b0673a34d0381d8b79efb27ee3f10a484655e5d9
Justin Turner <jmturner638@gmail.com>
2021-07-25 21:41:40 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,7 @@
 def main():
     #write your code below this line
-    message = input("Write a message...")
+    message = input("Write a message...\n")
+    print(message)
 
 if __name__ == '__main__':
     main()
